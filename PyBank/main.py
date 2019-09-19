5cfe2dd341e37de2015e9757f7b570118be2b339
Thu <tanguyen@knox.edu>
2019-09-18 20:19:25 -0500

    Update code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,85 @@
 #This is main.py in PyBank
+import os
+import csv
+
+budget_csv_path = os.path.join("Resources", "budget_data.csv")
+
+#default for greatest increase
+max_diff = 0
+#default value for greatest decrease
+min_diff = 0
+#net profit
+net_total = 0 
+#net change 
+net_diff = 0
+
+#empty list of dates
+date = [] 
+#empty list of profit/loss
+revenue = []   
+
+with open(budget_csv_path, newline="") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    
+    #skips header
+    next(csv_reader,None)
+
+    for row in csv_reader:
+        #define date
+        day = row[0]
+        #add date to array
+        date.append(day)
+
+        #define budget
+        budget = float(row[1])
+        #add budget to list
+        revenue.append(budget)
+        #compute net profit
+        net_total += budget
+
+    num_month = len(date)
+    print(num_month)
+    print("Financial Analysis")
+    print("-"*30)
+    print(f'Total Months: {num_month}')
+    print(f'Total: $ {net_total}')
+
+    #Compute change over a period
+    for i in range(1,num_month):
+
+        next_rev = revenue[i]
+        curr_rev = revenue[i-1]
+        diff = next_rev-curr_rev 
+        net_diff = net_diff + diff
+
+    #Find greatest increase
+    for i in range(1,num_month):
+        next_rev = revenue[i]
+        curr_rev = revenue[i-1]
+        diff = next_rev-curr_rev 
+        if max_diff < diff:
+            max_diff = diff
+            #assign date
+            date1= date[i]
+
+    #Find greatest decrease
+    for i in range(1,num_month):
+        next_rev = revenue[i]
+        curr_rev = revenue[i-1]
+        diff = next_rev-curr_rev 
+        if min_diff > diff:
+            min_diff = diff 
+            #assign date 
+            date2 = date[i]        
+    
+    #Average change
+    print(f'Average Change: ${net_diff/(num_month-1)}')
+    #Greatest increase in profit
+    print(f'Greatest Increase in Profits:{date1} ${max_diff}')
+    #Greatest decrease in profit
+    print(f'Greatest Decrease in Profits:{date2} ${min_diff}')
+
+
+         
+
+        
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,51 @@
 #This is main.py in PyPoll
+import os
+import csv
+
+#creating path
+election_csv_path = os.path.join("Resources", "election_data.csv")
+
+#set number of votes cast to 0
+tot_votes = 0
+#set popular vote to 0
+max_vote = 0
+#default for winner
+winner = ""
+
+# Initialize empty dictionaries
+election_dict = {}
+
+
+with open(election_csv_path, newline="") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    
+    #skip header
+    next(csv_reader,None)
+    
+    for row in csv_reader:
+        #assign candidate name
+        cand = row[2]
+        #increment votes cast
+        tot_votes += 1
+        #checks to see if candidate name is a key in dictionary
+        if cand in election_dict.keys():
+            # increment vote count for candidate
+            election_dict[cand] += 1
+        else:
+            # set voter count to 1
+            election_dict[cand] = 1
+    print("Election Results")
+    print("-"*30)
+    print(f'Total Votes: {tot_votes}')
+    print("-"*30)
+    for k,v in election_dict.items():
+        print(f'{k}: {round(v*100/tot_votes,3)}% ({v})')
+        #Find winner
+        if max_vote < v:
+            max_vote = v 
+            winner = k     
+    print("-"*30)
+    print(f'Winner: {winner}')
+    print("-"*30)
+
+
